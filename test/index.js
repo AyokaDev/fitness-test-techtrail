d1061a1e88f61c402f907635a7afbdf2c53dae2d
AyokaDev <102034372+AyokaDev@users.noreply.github.com>
2023-04-04 10:44:57 -0700

    Delete test directory

--- test/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("one").style.background = "red";
-document.getElementById("two").style.background = "blue";
-document.getElementById("three").style.background = "orange";
-document.getElementById("four").style.background = "green";
-document.getElementById("five").style.background = "brown";
-document.getElementById("six").style.background = "pink";
-document.getElementById("seven").style.background = "purple";
-document.getElementById("eight").style.background = "aqua";
